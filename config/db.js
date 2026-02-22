80b8661db33818e0e42861b1181a9feb427cefd0
Chahla <chahlabzd1@gmail.com>
2026-02-22 21:50:05 +0100

    Delete config directory

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('school_db', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql', // أو postgres إذا تستخدمين PostgreSQL
-  logging: false
-});
-
-module.exports = sequelize;
